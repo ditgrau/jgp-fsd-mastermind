8b98d5d086d1382a9d47ac39e4051103826f1a0e
DitGrau <juditgraup@gmail.com>
2023-05-18 00:05:46 +0200

    colors page and README

--- README.md
+++ README.md
@@ -4,12 +4,38 @@
 __<p align="center">Proyecto 3 - Mastermind - Semana 4</p>__
 
 
-<p align="center">Parte del Bootcamp de Full Stack Developer de Geekshubs Academy.</p>
+<p align="center">Implementación en JavaScript del juego Mastermind, como parte del Bootcamp de Full Stack Developer de Geekshubs Academy.</p>
 <p 
 
 <table align="center">
 <tr>
-<td><b>🚧 Work in progress🚧
+<td><b>🚧 Work in progress🚧</b>
 </td>
 </tr>
 </table> 
+
+
+
+
+### Bienvenido
+
+En la vista de inicio, encontramos dos botones que nos dirigen: el primero hacia el flujo del juego y el segundo a las instrucciones del juego.
+
+### Flujo del juego
+
+En primer lugar, nos encontramos con un pequeño formulario donde podemos introducir el nombre de usuario. Una vez ingresado este dato, en el mismo documento HTML tendremos la opción de seleccionar el nivel. El siguiente paso será personalizar los colores con los que deseamos jugar, y finalmente llegaremos al tablero de juego.
+
+#### Tablero de juego
+
+Al iniciar una nueva partida, se mostrará el tablero vacío donde el jugador podrá introducir su combinación. A un lado del tablero se mostrarán las bolas con los colores disponibles para realizar la jugada (elegidos previamente).
+
+El número de colores para jugar y el máximo de intentos variarán según el nivel seleccionado. Después de cada jugada, habrá un botón que permitirá verificar cuántos colores se han acertado.
+
+La solución del juego se mantendrá oculta y se mostrará al finalizar la partida, ya sea cuando el usuario haya ganado o cuando se hayan agotado los intentos y haya perdido.
+
+
+#### Niveles: 
+- "Principiante", por defecto: 4 colores posibles y 10 intentos.
+- "Intermedio": 8 intentos y 5 colores para elegir. 
+- "Avanzado": habrá 6 intentos y 6 colores para combinar.
+
--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const changeToLevel = ()=> {
+const changeToLevel = () => {
     // recoger el dato del user
     // let user = document.getElementById(userName).value;
     // sessionStorage.setItem('user', user);
-    
+
     // cambiar a la pagina nivel
     document.getElementById("mm-character").style.display = "none";
     document.getElementById("mm-levels").style.display = "block";
@@ -12,12 +12,29 @@ const changeToLevel = ()=> {
 
 // let user = document.getElementById("userName").value;
 //   console.log("Nombre del jugador: " + user);
+// {/* <input type="submit"></input> */} metinedo el submit en el form?
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    {
-        i++
-        
-    }
-    
-}
+
+/* En la esquina superior derecha, se mostrarán los colores disponibles, 
+los cuales serán variables y dependerán del nivel elegido por el jugador 
+(por defecto, serán 4). Para esto, se utilizará un procedimiento llamado 
+"crear_colores" que generará dinámicamente los elementos llamados "bolacolores". 
+Cada uno de estos elementos obtendrá su color de un vector previamente 
+creado e inicializado llamado "Colores".
+
+En la esquina inferior derecha, se mostrará la solución del juego. 
+Inicialmente, este panel no será visible. Se implementará un procedimiento 
+único llamado "crear_solucion" que creará en tiempo de ejecución los elementos 
+"bolasolucion" necesarios. El color de cada uno de estos elementos se seleccionará 
+aleatoriamente del vector "Colores" mencionado anteriormente. Es importante destacar 
+que los colores de la solución podrán repetirse. Sería recomendable almacenar 
+la solución en un vector aparte. */
+
+
+/* Cada vez que se inicie una nueva partida, se mostrarán los componentes vacíos 
+para que el jugador pueda ingresar su combinación. Para esto, se implementará un 
+procedimiento único llamado "crear_linea_bola" que generará dinámicamente los 
+elementos llamados "bola" según sea necesario. El número de estos componentes será 
+una constante con valor 4.*/
+
+// También será necesario implementar un procedimiento llamado "borrar_componentes" que permita eliminar todos los componentes creados durante el juego cuando se seleccione la opción de Nuevo Juego.
