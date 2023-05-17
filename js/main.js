92ac386793c006b9ae9dc252abac40b57d197dd3
DitGrau <juditgraup@gmail.com>
2023-05-17 16:29:54 +0200

    working on levels page design

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 const changeToLevel = ()=> {
+    // recoger el dato del user
+    // let user = document.getElementById(userName).value;
+    // sessionStorage.setItem('user', user);
+    
+    // cambiar a la pagina nivel
     document.getElementById("mm-character").style.display = "none";
     document.getElementById("mm-levels").style.display = "block";
-}
+};
+// TODO  revisar esto en el codigo de Felipe
+
