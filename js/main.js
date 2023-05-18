bb1e5f20e4cd574c95b4760ff3135fe46f98201f
DitGrau <juditgraup@gmail.com>
2023-05-18 18:44:48 +0200

    some js ideas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-// esta funcion almacena el dato del nombre //
 
-
-// const changeToLevel = () => {
-//     let user = document.getElementById("userName").value;
-//     sessionStorage.setItem('user', user);
-
-// 
 
 // Funciones para la seleccion de colores segun nivel //
 const bubbles4 = () => {
@@ -33,63 +26,21 @@ const changeNavbarLevel = () => {
     document.getElementById("nb-level").style.display = "block";
 };
 
-// Obtiene el elemento HTML al que se desea agregar el event listener
 
-const boton = document.getElementsByClassName("idboton");
-console.log (boton);
-// Define la función que se ejecutará cuando ocurra el evento
-// function miFuncion() {
-  // Código a ejecutar cuando se haga clic en el elemento
-//   console.log("Se hizo clic en el elemento");
-// }
-// Agrega el event listener al elemento
-// boton.addEventListener("click");
+// esta funcion almacena el dato del nombre //
 
 
-// const modePVP = () => {
-//     document.getElementById("electionMode-p").classList.add("hidden");
-//     document.getElementById("electionMode-cpu").classList.add("hidden");
-//     document.getElementById("nombres-home-vampires").classList.remove("hidden");
-//     document.getElementById("nombres-home-werewolves").classList.remove("hidden");
-//     document.getElementById("botonPlayers").classList.remove("hidden")
-// TODO  revisar esto en el codigo de Felipe
+// const changeToLevel = () => {
+//     let user = document.getElementById("userName").value;
+//     sessionStorage.setItem('user', user);
 
-/* estas constantes identifican los botones de nivel*/
-
-// let level1 = document.getElementById('levelOne');
-// let level2 = document.getElementById('levelTwo');
-// let level3 = document.getElementById('levelThree');
-
-
-// const bubblesLevel = () => {
-//     let level = document.getElementById("userName").value;
-//     sessionStorage.setItem('level', level);
-
-//     document.getElementById("mm-levelOneBubbles").style.display = "none";
-//     document.getElementById("mm-levelTwoBubbles").style.display = "block";
-// }
-
-// mm-levelBubbles
-// mm-levelTwoBubbles
-// mm-levelThreeBubbles
-// estan todas hidden 
-
-// Agrega un controlador de eventos a cada botón
-// boton1.addEventListener('click', handleClick);
-// boton2.addEventListener('click', handleClick);
-// boton3.addEventListener('click', handleClick);
-
-// Función manejadora del evento de clic
-// function handleClick(event) {
-  // Obtén el id del botón en el que se hizo clic
-//   const botonId = event.target.id;
-  
-  // Imprime el id del botón en la consola
-//   console.log('Se hizo clic en el botón con id:', botonId);
+// 
 
 
 
 
+
+// revisar esto que he hecho con mara
 
 
 // const changeToLevel = (id1, id2) => {
@@ -111,28 +62,3 @@ console.log (boton);
 // let user = document.getElementById("userName").value;
 //   console.log("Nombre del jugador: " + user);
 // {/* <input type="submit"></input> */} metinedo el submit en el form?
-
-
-/* En la esquina superior derecha, se mostrarán los colores disponibles, 
-los cuales serán variables y dependerán del nivel elegido por el jugador 
-(por defecto, serán 4). Para esto, se utilizará un procedimiento llamado 
-"crear_colores" que generará dinámicamente los elementos llamados "bolacolores". 
-Cada uno de estos elementos obtendrá su color de un vector previamente 
-creado e inicializado llamado "Colores".
-
-En la esquina inferior derecha, se mostrará la solución del juego. 
-Inicialmente, este panel no será visible. Se implementará un procedimiento 
-único llamado "crear_solucion" que creará en tiempo de ejecución los elementos 
-"bolasolucion" necesarios. El color de cada uno de estos elementos se seleccionará 
-aleatoriamente del vector "Colores" mencionado anteriormente. Es importante destacar 
-que los colores de la solución podrán repetirse. Sería recomendable almacenar 
-la solución en un vector aparte. */
-
-
-/* Cada vez que se inicie una nueva partida, se mostrarán los componentes vacíos 
-para que el jugador pueda ingresar su combinación. Para esto, se implementará un 
-procedimiento único llamado "crear_linea_bola" que generará dinámicamente los 
-elementos llamados "bola" según sea necesario. El número de estos componentes será 
-una constante con valor 4.*/
-
-// También será necesario implementar un procedimiento llamado "borrar_componentes" que permita eliminar todos los componentes creados durante el juego cuando se seleccione la opción de Nuevo Juego.
---
+++ js/prueba.js
@@ -0,0 +1,61 @@
+// RETO 1 - PAGINA COLORES 
+
+/* Pintar las bubbles de los colores que se eligen en el color picker */
+
+
+// RETO 2 - PAGINA COLORES 
+
+/* Tengo 4 elementos en el DOM, las bolas, cuando esten pintadas, quiero que aparezca el boton de comenzar juego"
+Tengo que hacer una escucha por cada elemento, comprobar que su valor es 1=0 y cuando se cumpla, mostrar el boton
+Los tengo que meter en un array y hacer un bucle (?) habra algo mas facil o mas corto ?*/
+
+
+// RETO
+
+/* Hacer un borrado del session storage cuando se hace clic en nueva partida
+un procedimiento llamado "borrar_componentes" (?) */
+
+
+// RETO
+
+// Hacer session storage de nombre 
+// Hacer session storage de nivel 
+// Hacer session storage de colores 
+
+
+// RETO
+
+// Trasladar toda la info del session storage al html del tablero 
+
+
+// RETO
+
+// Tablero dinamico utilizando appendChild (?)
+
+
+/*Crear la vista de juego:
+
+Diseñar y crear la página principal del juego.
+Tablero con bolas de colores y espacio para blancas y negras.
+Mostrar las bolas de colores seleccionables por el jugador.
+Combinación aleatoria en oculto.
+Agregar un menú con las opciones: "Partida nueva" y "Salir".
+
+Lógica del juego:
+
+Generar una combinación aleatoria de colores según el nivel elegido por el jugador.
+(un array)
+
+Permitir al jugador seleccionar los colores para crear su combinación y mostrarla en pantalla.
+(otro array)
+
+Implementar el botón de comprobación para verificar la cantidad de aciertos.
+- Mostrar bolas blancas para indicar los aciertos de colores correctos pero en posiciones incorrectas.
+- Mostrar bolas negras para indicar los aciertos de colores y posiciones correctas.
+Comprobar si el jugador ha ganado o agotado los intentos.
+
+Implementar la lógica de fin de partida:
+
+Mostrar un mensaje de victoria si el jugador ha adivinado la combinación.
+Mostrar la solución si el jugador ha agotado los intentos.
+Reiniciar el juego al hacer clic en "Partida nueva" para empezar de cero.*/
