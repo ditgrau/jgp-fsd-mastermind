76a4bef6a46631d4ce7f6e0b6819bf2cb28899b9
DitGrau <juditgraup@gmail.com>
2023-05-21 16:51:54 +0200

    working on gameboard construction function

--- js/game.js
+++ js/game.js
@@ -1,43 +1,35 @@
 
 
-                    // let elementoExistente = document.getElementById('FilaTablero');
-                    // console.log (elementoExistente);
-                    
-                    // let padresito = document.getElementById('contenedor');
-                    // console.log (padresito);
-                    
-                    // let numerodefilas = Math.floor(Math.random() * 11);
-                    // for (let i = 0; i < numerodefilas; i++) {
-                    //     let elementoClonado = elementoExistente.cloneNode(true);
-                    //     padresito.appendChild(elementoClonado);
-                    // }
-
-// let rowGameboard = document.getElementById('rowGameboard');
-// console.log (rowGameboard);
-
-// let gameBoard = document.getElementById('gameBoard');
-// console.log (gameBoard);
-
-// let numberRows = Math.floor(Math.random() * 11);
-// for (let i = 0; i < numberRows; i++) {
-//     let clonedRow = rowGameboard.cloneNode(true);
-//     gameBoard.appendChild(clonedRow);
-// }
-
-// esta con bucle aleatorio, tengo que hacer un if para cada nivel
-
-// codigo ejercicio de clase
-// let numeroParrafos = Math.floor(Math.random() * 11);
+// let elementoExistente = document.getElementById('FilaTablero');
+// console.log (elementoExistente);
 
 // let padresito = document.getElementById('contenedor');
+// console.log (padresito);
 
-// for (let i = 0; i < numeroParrafos; i++) {
-//     let hijito =  document.createElement("p");
-//     hijito.innerHTML = `Somos una fokin famili`;
-//     padresito.appendChild(hijito);  
+// let numerodefilas = Math.floor(Math.random() * 11);
+// for (let i = 0; i < numerodefilas; i++) {
+//     let elementoClonado = elementoExistente.cloneNode(true);
+//     padresito.appendChild(elementoClonado);
 // }
 
-// aqui la logica del juego
+let rowGameboard = document.getElementById('rowGameboard');
+console.log(rowGameboard);
 
-// 
+let gameBoard = document.getElementById('gameBoard');
+console.log(gameBoard);
 
+const cloneRows = (numberRows) => {
+    for (let i = 0; i < numberRows; i++) {
+        let clonedRow = rowGameboard.cloneNode(true);
+        gameBoard.appendChild(clonedRow);
+    }
+}
+let userLevel = 10 //acabr esta funcion
+
+if (userLevel === "level") {
+    cloneRows(10);
+} else if (userLevel === "level++") {
+    cloneRows(8);
+} else {
+    cloneRows(6);
+}
