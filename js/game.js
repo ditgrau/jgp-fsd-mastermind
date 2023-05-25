d2350f3903ca26cd6e67f785794573c210ca4da7
DitGrau <juditgraup@gmail.com>
2023-05-25 22:42:16 +0200

    first cell traverses the array

--- js/game.js
+++ js/game.js
@@ -1,22 +1,20 @@
 
-// level board - bubbles colored
 
-let selectedColors = JSON.parse(sessionStorage.getItem('newColors'));
+//level board // chosen colors
+
 let divSelectedColors = document.getElementById('levelBoard');
-let selectedLevel = sessionStorage.getItem('level');
-console.log (selectedColors);
+let selectedColors = JSON.parse(sessionStorage.getItem('newColors'));
 
 const createLevelBoard = () => {
     for (let i = 0; i < selectedColors.length; i++) {
         let clonedSelector = document.createElement("div");
         clonedSelector.classList.add('circleColorBoard');
         clonedSelector.style.backgroundColor = selectedColors[i];
-        // clonedSelector[i].addEventListener('click', clickSelector());
         divSelectedColors.appendChild(clonedSelector); 
     }
 }
-
 createLevelBoard();
+
 
 // secret combination
 
@@ -27,11 +25,9 @@ const colorsRandom = () => {
         let randomIndex = Math.floor(Math.random() * selectedColors.length);
         secretCombo.push(selectedColors[randomIndex]);
     }
-    console.log (secretCombo);
-    console.log (selectedColors);
-    
-};
+}
 colorsRandom();
+
 
 //  secret board
 
@@ -46,14 +42,20 @@ const createInsightBoard = () => {
         divInsightColors.appendChild(insightBubble); 
     }
 } 
-
 createInsightBoard();
 
 
+// GAMEBOARD 
+
+let selectedLevel = sessionStorage.getItem('level');
+let gameBoard = document.getElementById ("gameBoard");
+
+// Create rows // cells // snitches
 
 const createRows = (numberRows) => {
 
     for (let i = 0; i < numberRows; i++) {
+
         let rowsGameboard = document.createElement("div");
         rowsGameboard.classList.add("row");
         rowsGameboard.id = `rowGameboard-${i}`;
@@ -74,85 +76,32 @@ const createRows = (numberRows) => {
                 let cellsrow = document.createElement("div");
                 cellsrow.classList.add("circleColorBoard");
                 rowsGameboard.appendChild(cellsrow);
-                cellsrow.classList.add(`celda${i}`);
-                cellsrow.dataset.row = i;
+                cellsrow.id = `cell-${j}-row-${i}`;
             }
     }
 }
-
 createRows (selectedLevel);
 
-const firstRow = document.getElementById("rowGameboard-0");
-console.log(firstRow.id);
-firstRow.style.backgroundColor = "red";
-const firstCell = document.getElementsByClassName(`celda0`);
-// es un html collection // convertir a array
-let arrayRow = Array.from(document.getElementsByClassName('celda0'));
-// les tengo que dar la porpiedad de clicar
-// miElemento.addEventListener("click", function() {
-//     alert("¡Has hecho clic en el elemento!");
-//   });
-arrayRow[i].addEventListener('click', (mostrarColorArray)=>{})
 
-// selectedColors mi array de los colores elegidos
-// si utilizo bucle no me saldria con cada clic, tengo que utilizar if .
-let numeroClic = 0;
-const mostrarColorArray = () => {
-    if (numeroClic>selectedColors.lenght) {
-        return;
+
+
+// esta funcion recorre el array de colores, ME SIRVE!!
+
+
+
+let contador = 0;
+const pintarBolas = () => {
+    console.log("clic en el botón");
+    if (contador<selectedColors.length) {
+        clicable.style.backgroundColor = selectedColors[contador];
+        contador ++;
+    }else {
+        contador = 0;
     }
-    selectedColors[numeroClic];
-    numeroClic++;
-};
-// tengo dos arrays, clicando sobre el primer elemento del primer array tengo que recorrer el segundno.
+    
+    }
+
+let clicable = document.getElementById("cell-0-row-0")
+clicable.addEventListener('click', pintarBolas);
 
 
-
-// Agregar el evento de clic al botón
-boton.addEventListener('click', mostrarSiguienteElemento);
-
-
-
-
-
-// .some, si algun elemento del array cumple la condicion: estar dentro del array dos
-
-// const array = [1, 2, 3, 4, 5];
-// let currentIndex = 0;
-
-// function mostrarSiguienteElemento() {
-//   if (currentIndex < array.length) {
-//     const elementoActual = array[currentIndex];
-//     console.log(elementoActual);
-//     currentIndex++;
-//   } else {
-//     console.log("Se han mostrado todos los elementos del array.");
-//   }
-// }
-
-// // Agrega un evento onclick al documento o a un elemento específico
-// document.onclick = mostrarSiguienteElemento;
-
-// let 
-
-// selectedColors // es mi array que recorre el click
-// let contador = 0;
-
-// const clickIteratesColors = () => {
-//     if (contador < selectedColors.length) {
-//         contador ++
-//         let colorShown = selectedColors[contador];
-
-//     }
-// }
-
-
-
-
-let arrayVacio = [];
-let arraylleno = ['pep', 'tom', 'mara', 'marta'];
-
-const clearArray = () => {
-    arraylleno = arrayVacio;
-    return 
-};
