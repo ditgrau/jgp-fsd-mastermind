77259714bc6fbf35d0deaee42254e6d3bc5635de
DitGrau <juditgraup@gmail.com>
2023-05-26 08:30:09 +0200

    feature: activate rows with click

--- js/game.js
+++ js/game.js
@@ -113,7 +113,29 @@ createRows (selectedLevel);
 
 //////////////////// esta funcion recorre con todas las bolitas de la msima fila
 
+// let numeroClics = 0;
+// let clicablearray = Array.from(document.getElementsByClassName("mirow-6"));
+
+// const pintarBolas = (clicable) => {
+//     console.log("clic en el botón");
+//     if (numeroClics<selectedColors.length) {
+//         clicable.style.backgroundColor = selectedColors[numeroClics];
+//         numeroClics ++;
+//     }else {
+//         numeroClics = 0;
+//     }
+// }
+
+// clicablearray.forEach((clicable)=> {
+//     clicable.addEventListener('click',() => pintarBolas(clicable));
+// });
+
+
+/////////////////////////////////////
+
 let numeroClics = 0;
+let rowContador = 0;
+let clicablearray = Array.from(document.getElementsByClassName(`mirow-${rowContador}`));
 
 const pintarBolas = (clicable) => {
     console.log("clic en el botón");
@@ -124,43 +146,86 @@ const pintarBolas = (clicable) => {
         numeroClics = 0;
     }
 }
-
-console.log(arrayByRows);
-let clicablearray = Array.from(document.getElementsByClassName("mirow-0"));
 clicablearray.forEach((clicable)=> {
-    clicable.addEventListener('click',() => pintarBolas(clicable));
+            clicable.addEventListener('click',() => pintarBolas(clicable));
+        });
+console.log(rowContador);
 
-});
 
-
-/////////////////////////////////////
-
-// check button and delete
-
-let checkButton = document.getElementById('checkButton');
-let dismissButon = document.getElementById('dismissButon');
-
-let arrayByRows = [];
-const saveCombi = ()=> {
-
+const desbloquearFila = ()=>{
+    rowContador++
+    console.log (rowContador);
+    clicablearray = Array.from(document.getElementsByClassName(`mirow-${rowContador}`));
+    clicablearray.forEach((clicable)=> {
+        clicable.addEventListener('click',() => pintarBolas(clicable));
+    });
 }
-saveCombi ()
-checkCombi ()
-dismissCombi()
-/////////////////////////////////
 
 
-function tienenElementoComun(array1, array2) {
-    return array1.some(elemento => array2.includes(elemento));
-  }
+
+// const checkCombi = () => {
+    
+//     //// aqui tengo que meter mmi codigo de comprobacion de colores o invocar a la funcion
+//     console.log("estoy aqui");
+//     rowContador++
+//     clicableArray = Array.from(document.getElementsByClassName(`mirow-${rowContador}`));
+//     numeroClics = 0;
+//    
+// }
+
+// do {
+//     clicablearray.forEach((clicable)=> {
+//         clicable.addEventListener('click',() => pintarBolas(clicable));
+//     });
+    
+// } while (false);
+
+
+
+// let rowContador = 0;
+
+
+
+
+
+
+
+
+
+
+// // check button and delete
+
+// let checkButton = document.getElementById('checkButton');
+// let dismissButton = document.getElementById('dismissButton');
+
+
+// const saveCombi = ()=> {
+//     let arrayToCompare = [];
+//     for (var clicable of clicablearray) {
+//     var colorToCompare = clicable.style.backgroundColor;
+//     arrayToCompare.push(colorToCompare);
+//     console.log(arrayToCompare);
+//     }
+//     console.log(arrayToCompare);
+// }
+
+// saveCombi ()
+// checkCombi ()
+// dismissCombi()
+// /////////////////////////////////
+
+
+// function tienenElementoComun(array1, array2) {
+//     return array1.some(elemento => array2.includes(elemento));
+//   }
   
-  // Ejemplo de uso
-  const array1 = [1, 2, 3, 4];
-  const array2 = [5, 6, 3, 8];
+//   // Ejemplo de uso
+//   const array1 = [1, 2, 3, 4];
+//   const array2 = [5, 6, 3, 8];
   
-  if (tienenElementoComun(array1, array2)) {
-    console.log("Los arrays tienen al menos un elemento en común.");
-  } else {
-    console.log("Los arrays no tienen elementos en común.");
-  }
+//   if (tienenElementoComun(array1, array2)) {
+//     console.log("Los arrays tienen al menos un elemento en común.");
+//   } else {
+//     console.log("Los arrays no tienen elementos en común.");
+//   }
   
