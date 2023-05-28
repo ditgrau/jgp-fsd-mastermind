3213eb4a5e3cfd1c12a7f7010a74890ffc0002d0
DitGrau <juditgraup@gmail.com>
2023-05-28 21:11:23 +0200

    check function already paints snitches

--- js/game.js
+++ js/game.js
@@ -139,107 +139,7 @@ const compareArray = () => {
 };
 
 
-// incluyendo posicion
-
-
-
-
-
-
-// const firstMatch = () => {
-
-//     console.log (captureRandom);
-//     console.log (currentArray);
-
-//     let blackWhiteElements = [];
-//     let commonElements = [];
-
-//     for (let i = 0; i < 5; i++) {
-//     if (currentArray.includes(captureRandom[i]) && !commonElements.includes(captureRandom[i])) {
-//         if (captureRandom[i] === currentArray[i]) {
-//             blackWhiteElements.push('#FF0000');
-//         }else {
-//             blackWhiteElements.push('#FFFFFF');
-//         }
-//         }
-//         console.log (blackWhiteElements);
-//     }
-
-
-
-
-
-
-    // const firstMatch = () => {
-
-    //     console.log (captureRandom);
-    //     console.log (currentArray);
-    
-    //     let blackWhiteElements = [];
-    //     let commonElements = [];
-    
-    //     for (let i = 0; i < 5; i++) {
-    //     if (currentArray.includes(captureRandom[i]) && (captureRandom[i] === currentArray[i])) {
-    //         commonElements.push(captureRandom[i]);
-    //         blackWhiteElements.push('#FFFFFF');
-    //         }else if (currentArray.includes(captureRandom[i]) && !commonElements.includes(captureRandom[i])) {
-    //             blackWhiteElements.push('#FF0000');
-    //         }
-    //         }
-    //         console.log (blackWhiteElements);
-    //     }
-    
-        
-
-
-    // const firstMatch = () => {
-
-    //     console.log (captureRandom);
-    //     console.log (currentArray);
-    
-    //     let blackWhiteElements = [];
-    //     let commonElements = [];
-    
-    //     for (let i = 0; i < 5; i++) {
-    //     if (currentArray.includes(captureRandom[i]) && (captureRandom[i] === currentArray[i])) {
-    //         currentArray[i] = null;
-    //         blackWhiteElements.push('#FFFFFF');
-    //         }else if (currentArray.includes(captureRandom[i])) {
-    //             blackWhiteElements.push('#FF0000');
-    //         }
-    //         }
-    //         console.log (blackWhiteElements);
-    //     }
-    
-    // console.log (firstMatch());
-
-
-    // const firstMatch = () => {
-    //     let blackWhiteElements = [];
-    //     let newCurrent = currentArray.slice();
-    //     let newCapture = captureRandom.slice();
-
-    //     for (let i = 0; i < newCapture.length; i++) {
-    //     if (newCapture.includes(newCurrent[i]) && (newCurrent[i] === newCapture[i])) {
-    //         blackWhiteElements.push('#FFFFFF');
-    //         newCurrent[i] = null;
-    //         newCapture[i] = null;
-    //         console.log (newCurrent);
-    //         console.log (newCapture);
-         
-    //         }else if (newCapture.includes(newCurrent[i])) {
-    //         blackWhiteElements.push('#FF0000');
-    //         newCurrent[i] = null;
-    //         console.log (newCurrent);
-    //         console.log (newCapture);
-            
-    //         }
-    //     }
-    //     console.log(blackWhiteElements);
-    //     }
-
-    //     firstMatch();
-
+// check function
 
     const firstMatch = () => {
         let blackWhiteElements = [];
@@ -250,7 +150,7 @@ const compareArray = () => {
         if (newCapture.includes(newCurrent[i]) && newCurrent[i] === newCapture[i]) {
             newCurrent[i] = null;
             newCapture[i] = null;
-            blackWhiteElements.push('#FFFFFF');
+            blackWhiteElements.push("#000000");
             console.log(newCurrent);
             console.log(newCapture);
             }
@@ -262,7 +162,7 @@ const compareArray = () => {
             if (index > -1) {
             newCapture[index] = null;
             }
-            blackWhiteElements.push('#FF0000');
+            blackWhiteElements.push('#FFFFFF');
             newCurrent[j] = null;
             console.log(newCurrent);
             console.log(newCapture);
@@ -270,25 +170,20 @@ const compareArray = () => {
         }
 
         console.log(blackWhiteElements);
+
+const snitchesBoard = () => {
+    let colorSnitches = blackWhiteElements.sort();
+    for (let i = 0; i < colorSnitches.length; i++) {
+    let snitches = snitchesArray[i];
+    let colorBn = colorSnitches[i];
+    snitches.style.backgroundColor = colorBn;
     }
+};
 
-    firstMatch();
+snitchesBoard();
+};
 
-
-
-
-
-
-
-
-
-
-    
-
-    // let getWhite = whiteElements.length - blackElements.length;
-    // let blackWhite = blackElements.concat(getWhite);
-    // console.log (blackWhite);
-    // return blackWhite
+firstMatch();
 
 
 // const unblockSnitches = ()=>{
